be3f3fadc8dcd596ee8c3cac92d1eaf68678c98a
Natnael Abebe <natnaelabebe1994@gmail.com>
2024-02-16 19:30:26 +0300

    update

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "./footer";
-import Blog from "./Blog";
 export default function SharedLayout() {
   return (
     <div>
