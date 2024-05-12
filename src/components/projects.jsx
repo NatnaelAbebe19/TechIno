f178fcf02b45a7369391e3565f80401792a76fc7
jud <joejud@gmail.com>
2024-05-13 01:42:11 +0300

    update the animation on the scroll

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 import oneCard from "/oneCard.jpg";
 import Datas from "./Projectstdata";
+import { motion } from "framer-motion";
+
+const varianted = {
+  initial: {
+    opacity: 0,
+    x: -100,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: { delay: 0.15, duration: 0.35 },
+  },
+};
 
 export default function projects() {
   const [showMore, setShowMore] = useState(-1);
@@ -16,7 +29,14 @@ export default function projects() {
     <section className="project--container--main">
       <h1 className="porjects--header">Projects we recently developed</h1>
       {Datas.map((data, index) => (
-        <div className="project--container" key={index}>
+        <motion.div
+          className="project--container"
+          key={index}
+          variants={varianted}
+          initial="initial"
+          animate="animate"
+          viewport={{ once: true }}
+        >
           <div className="project--image--container">
             <img src={data.image} alt={data.title} className="project--image" />
 
@@ -41,7 +61,7 @@ export default function projects() {
               </div>
             )}
           </div>
-        </div>
+        </motion.div>
       ))}
     </section>
   );
